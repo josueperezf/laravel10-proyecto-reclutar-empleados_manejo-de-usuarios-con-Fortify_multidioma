ed280e0f44f2e42badcca16960b794a918c9ffcb
Josue perez <josueperezf@gmail.com>
2023-08-25 17:01:39 -0400

    ocultar acceso para crear o editar vacantes solo a reclutadores

--- app/Http/Controllers/VacanteController.php
+++ app/Http/Controllers/VacanteController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Vacante;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
 
 // los metodos que faltan, como por ejemplo el store lo borramos, ya que esa accion se hara con livewire , especificamente con la clase CrearVacante
 
@@ -14,6 +13,7 @@ class VacanteController extends Controller
      * Display a listing of the resource.
      */
     public function index() {
+        // AL POLICY NO LE ENVIAMOS EL USUARIO LOGUEADO PORQUE EL LO ACCEDE AUTOMATICAMENTE, LO QUE TENEMOS QUE PASAR  ES SI QUEREMOS ANALIZAR UNA VACANTE EN PARTICULAR O ALGO ASI
         // la sioguiente liena es para indicar que solamente tendra permiso los usuarios de rol 2, los reclutadores de ver el listado de vacantes http://localhost/dashboard
         $this->authorize('viewAny', Vacante::class); // este metodo ya se crea cuando generamos el policy
         return view('vacantes.index');
@@ -23,14 +23,16 @@ class VacanteController extends Controller
      * Show the form for creating a new resource.
      */
     public function create(): View {
+        // AL POLICY NO LE ENVIAMOS EL USUARIO LOGUEADO PORQUE EL LO ACCEDE AUTOMATICAMENTE, LO QUE TENEMOS QUE PASAR  ES SI QUEREMOS ANALIZAR UNA VACANTE EN PARTICULAR O ALGO ASI
+        $this->authorize('create', Vacante::class); //create es porque asi se llama el metodo de la clase VacantePolicy
         // la vista vacantes.create llama a un componente 'CrearVacante' que arma el formulario, es ese componente el que le pasa la data para listar categorias y demas
         return view('vacantes.create');
     }
 
-    /**
-     * Display the specified resource.
-     */
+
+    // LAS VACANTES SOLO LAS PUEDEN VER LOS DESARROLLADORES NO LOS RECLUTADORES, NO ES ALGO TANTO DE LOGICA SI NO PARA QUE SIRVA DE PRACTICA PARA ENTENDER. EN ESTA VISTA ES DONDE EL USUARIO ADJUNTA EL CURRICULO
     public function show(Vacante $vacante) {
+        // CUALQUIER PERSONA LOGUEADA O NO PODRA VER UNA VACANTE EN PARTICULAR
         return view('vacantes.show', compact('vacante'));
     }
 
@@ -38,6 +40,7 @@ class VacanteController extends Controller
      * Show the form for editing the specified resource.
      */
     public function edit(Vacante $vacante) {
+        // AL POLICY NO LE ENVIAMOS EL USUARIO LOGUEADO PORQUE EL LO ACCEDE AUTOMATICAMENTE, LO QUE TENEMOS QUE PASAR  ES SI QUEREMOS ANALIZAR UNA VACANTE EN PARTICULAR O ALGO ASI
         $this->authorize('update', $vacante); // se ejecuta el metodo update del policy VacantePolicy, no hay que decirle el nombre, ya que con el $this, sabe a que modelo pertenece
         return view('vacantes.edit', compact('vacante'));
     }
--- app/Policies/VacantePolicy.php
+++ app/Policies/VacantePolicy.php
@@ -9,10 +9,8 @@ use Illuminate\Auth\Access\Response;
 
 class VacantePolicy
 {
-    /**
-     * Determinar si el usuario puede ver algún modelo.
-     */
-    public function viewAny(User $user) {
+    // creo este metodo se relaciona siempre al metodo index del controlador, ya la logica la ponemos nosotros, pero debe retornar un bool
+    public function viewAny(User $user): bool {
         // si es reclutador si puede ver el panel de mis vacantes http://localhost/dashboard .este metodo lo aplicamos en el controlador 'VacanteController', metodo index
         return $user->rol == RolConst::RECLUTADOR;
     }
@@ -22,15 +20,15 @@ class VacantePolicy
      */
     public function view(User $user, Vacante $vacante)
     {
-        //
+
     }
 
     /**
      * Determinar si la usuario puede crear modelos.
      */
-    public function create(User $user)
-    {
-        //
+    // solo los reclutadores pueden crear vacantes. el internamente toma el usuario logueado
+    public function create(User $user): bool {
+        return $user->rol == RolConst::RECLUTADOR;
     }
 
     //* Verificamos si un usuario tiene el permiso de modificar una vacante. puede modificar la vacante solo quien la creó
