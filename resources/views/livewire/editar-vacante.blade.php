b445cecf03db4bf0d0c838c872e86b9b4db2e744
Josue perez <josueperezf@gmail.com>
2023-08-25 15:48:06 -0400

    cambiar el boton editar y guardar vacantes para que se deshabilite si hay un loading

--- resources/views/livewire/editar-vacante.blade.php
+++ resources/views/livewire/editar-vacante.blade.php
@@ -138,6 +138,19 @@
     --}}
     <div class="mt-4">
         <x-primary-button
+            wire:loading.attr="disabled"
+        >
+            {{ __('Edit vacancy') }} &nbsp;
+            <div wire:loading  class="animate-spin h-4 w-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+                </svg>
+            </div>
+
+        </x-primary-button>
+
+        {{-- el siguiente bloque comentado hace que el boton submit se deshabilite solo cuando precionen en guardar, pero si un usuario esta adjunta una foto y de una vez da guardar no guarda la foto porque aun no la habia cargado, entonces mejor no ponerla, la deje comentado porque sirve de ejemplo como comunicarse con metodo backend desde el html  --}}
+        {{-- <x-primary-button
             wire:click="cargador"
             wire:loading.attr="disabled"
             wire:target="cargador"
@@ -149,6 +162,6 @@
                 </svg>
             </div>
 
-        </x-primary-button>
+        </x-primary-button> --}}
     </div>
 </form>
